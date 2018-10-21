394b7311eca732cd92eccbd27559f00c5bedc917
Paulo <paulo.es@live.com>
2018-10-21 16:29:46 -0300

    Creating about, contact and post pages

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
 
 
 app.listen(4000, () => {
