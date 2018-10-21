70cfbe79ba9a9537e713a3f2cbd763222cebceea
Paulo <paulo.es@live.com>
2018-10-21 18:39:59 -0300

    Setting up template engine

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const path = require('path')
+const expressEdge = require('express-edge')
 
 const app = new express()
 
 app.use(express.static('public'))
+app.use(expressEdge)
+app.set('views', `${__dirname}/views`)
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 
 
